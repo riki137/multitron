5b8dc1cc91b54c28885ebf897915cfc9cdac02ac
riki137 <richard@popelis.sk>
2025-08-14 15:55:34 +0200

    increase test coverage

--- tests/Unit/TaskTreeBuilderTest.php
+++ tests/Unit/TaskTreeBuilderTest.php
@@ -6,6 +6,7 @@ namespace Multitron\Tests\Unit;
 use LogicException;
 use Multitron\Comms\TaskCommunicator;
 use Multitron\Execution\Task;
+use Multitron\Tree\Partition\PartitionedTaskInterface;
 use Multitron\Tree\TaskNode;
 use Multitron\Tree\TaskTreeBuilder;
 use Multitron\Tree\TaskTreeBuilderFactory;
@@ -43,6 +44,27 @@ final class TaskTreeBuilderTest extends TestCase
         $task = ($node->factory)();
         $this->assertInstanceOf(DummyTask::class, $task);
     }
+
+    public function testServiceMustImplementTask(): void
+    {
+        $container = new Container(new \Pimple\Container([
+            NotATask::class => fn() => new NotATask(),
+        ]));
+        $builder = new TaskTreeBuilder($container);
+        $node = $builder->service(NotATask::class);
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Service "' . NotATask::class . '" must implement Task interface');
+        ($node->factory)();
+    }
+
+    public function testPartitionedFactoryThrowsWithoutContainer(): void
+    {
+        $builder = new TaskTreeBuilder(null);
+        $node = $builder->partitioned(DummyPartitionedTask::class, 1);
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('TaskTreeBuilderFactory has no container injected');
+        ($node->children[0]->factory)();
+    }
 }
 
 final class DummyTask implements Task
@@ -50,4 +72,19 @@ final class DummyTask implements Task
     public function execute(TaskCommunicator $comm): void
     {
     }
+}
+
+final class DummyPartitionedTask implements PartitionedTaskInterface
+{
+    public function execute(TaskCommunicator $comm): void
+    {
+    }
+
+    public function setPartitioning(int $index, int $count): void
+    {
+    }
+}
+
+final class NotATask
+{
 }
